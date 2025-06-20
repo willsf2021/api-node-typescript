c396138205dbe5051530877b37e565ef1d0af7c3
willsf2021 <willsf2021@gmail.com>
2025-06-20 13:44:11 -0300

    feat: created and imported Router from express, separeted routes by a separated file, updated 'Server.ts', installed 'http-status-codes' dependence

---
+++ src/server/routes/index.ts
@@ -0,0 +1,16 @@
+import { StatusCodes } from "http-status-codes";
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (_, res) => {
+  return res.send("Olá, DEV!");
+});
+
+router.post("/teste", (req, res) => {
+  console.log(req.params);
+
+  return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
+});
+
+export { router };
