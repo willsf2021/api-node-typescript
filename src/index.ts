47e75681c873a82d9f2d1fdc6471bc3935d3fd91
willsf2021 <willsf2021@gmail.com>
2025-06-20 14:58:27 -0300

    feat: created '.env'

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { server } from "./server/Server";
 
-server.listen(3333, () => {
-  console.log("App rodando!");
+server.listen(process.env.PORT || 3333, () => {
+  console.log(`App rodando na porta ${process.env.PORT || 3333}!`);
 });
--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import "dotenv/config";
+
 import { router } from "./routes";
 
 const server = express();
